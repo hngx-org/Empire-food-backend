e9249467c180ccdccfcc271b5a511d6b1edafcb7
Wilson Icheku <ichekuwilson538@gmail.com>
2023-09-20 21:22:36 +0100

    user_schema update

--- app/schemas/user_schemas.py
+++ app/schemas/user_schemas.py
@@ -2,16 +2,25 @@ from pydantic import BaseModel, EmailStr
 
 
 class UserCreate(BaseModel):
-  email:str
-  password: str
-  first_name: str
-  last_name: str
-  phone_number: str
+    email: str
+    password: str
+    first_name: str
+    last_name: str
+    phone_number: str
 
 
-class UserResponseSchema(BaseModel):
-  id:str
-  email:str
-  name: str
-  access_token: str
-  is_admin: bool
+class UserLoginResponse(BaseModel):
+    id: int
+    email: EmailStr
+    access_token: str
+    refresh_token: str
+    is_admin: bool
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class TokenRequest(BaseModel):
+    refresh_token: str
