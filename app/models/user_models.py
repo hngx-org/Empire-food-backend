d844118691feb566255eb0b3635c41b9c052a65a
Victor Ajayi <81755664+victor-ajayi@users.noreply.github.com>
2023-09-20 15:23:03 +0300

    chore: change user model name

--- app/models/user_models.py
+++ app/models/user_models.py
@@ -2,23 +2,23 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from uuid import uuid4 as uuid
 from datetime import datetime
 
-class Users():
-  __tablename__ = 'users'
-  id = Column(Integer, primary_key=True, default=uuid)
-  name = Column(String(50))
-  email = Column(String(50))
-  phonenumber = Column(String(50))
-  password_hash = Column(String(50))
-  org_id = Column(Integer, ForeignKey('organization.id'))
-  refresh_token = Column(String(255))
-  created_at = Column(datetime.utcnow())
-  updated_at = Column(datetime.utcnow())
 
-
-  class withdrawals():
-    __tablename__ = 'withdrawals'
+class User:
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True, default=uuid)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    status = Column(String(50))
-    amount = Column(Integer)
+    name = Column(String(50))
+    email = Column(String(50))
+    phonenumber = Column(String(50))
+    password_hash = Column(String(50))
+    org_id = Column(Integer, ForeignKey("organization.id"))
+    refresh_token = Column(String(255))
     created_at = Column(datetime.utcnow())
+    updated_at = Column(datetime.utcnow())
+
+    class withdrawals:
+        __tablename__ = "withdrawals"
+        id = Column(Integer, primary_key=True, default=uuid)
+        user_id = Column(Integer, ForeignKey("users.id"))
+        status = Column(String(50))
+        amount = Column(Integer)
+        created_at = Column(datetime.utcnow())
