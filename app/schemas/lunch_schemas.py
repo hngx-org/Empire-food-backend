de8ef6974672ad98226debbc80b5032c3f64f5df
psych0wh1z <ikeagudike@gmail.com>
2023-09-21 18:56:15 +0100

    Add GetLunchResponse with LunchResponseData schema

--- app/schemas/lunch_schemas.py
+++ app/schemas/lunch_schemas.py
@@ -1,20 +1,20 @@
 from pydantic import BaseModel
 
 
-class LunchData(BaseModel):
-    receiver_id: id,
-	sender_id: id,
+class LunchResponseData(BaseModel):
+    receiver_id: int,
+    sender_id: int,
     quantity: int,
-	redeemed: bool,
-	note: str,
-	created_at: str,
-	id: str
+    redeemed: bool,
+    note: str,
+    created_at: str,
+    id: int
 
 
 class GetLunchResponse(BaseModel):
     message: str,
     status_code: int,
-    data: LunchData
+    data: LunchResponseData
 
     class Config:
         json_schema_extra = {
@@ -24,11 +24,11 @@ class GetLunchResponse(BaseModel):
                 "data": {
                     "receiverId": 11,
                     "senderId": 7,
-                    "quantity": 2,
+                    "quantity": 5,
                     "redeemed": False,
                     "note": "Special instructions for the lunch",
-                    "created_at": "2023-09-21T15:56:41.716282",
-                    "id": 55
-	            }
+                    "created_at": '2023-09-21T15:56:41.716282',
+                    "id": 22
+                }
             }
         }
