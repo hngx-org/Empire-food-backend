b036293fe8b066395e0ec551d140bd8d82600935
psych0wh1z <ikeagudike@gmail.com>
2023-09-21 18:56:38 +0100

    Refactor lunch_services method

--- app/services/lunch_services.py
+++ app/services/lunch_services.py
@@ -1,9 +1,10 @@
+from app.db.lunch_db import view_lunch
 from sqlalchemy.orm import Session
-from app.schemas.user_schemas import UserCreate
-from app.models.lunch_models import Lunches
 
-def get_lunch(db:Session, lunch_id:int):
-    
-    lunch = db.query(Lunches).filter(lunch_id == Lunches.id).first()
 
+def fetch_lunch(db: Session, lunch_id: int):
+    # Perform check for null lunch_id value
+    if not lunch_id:
+        return False
+    lunch = view_lunch(db, lunch_id)
     return lunch
