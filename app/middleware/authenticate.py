4aac24a8e248796639bf050ced81f0ac072ad320
ackmedtijani <tijanioboiriendev@gmail.com>
2023-09-21 12:07:51 +0100

    Update the jwt_hanlder and authenticate dependency function

--- app/middleware/authenticate.py
+++ app/middleware/authenticate.py
@@ -1,15 +1,17 @@
-from fastapi import HTTPException, Request
-from .jwt_handler import verify_access_token
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+from .jwt_handler import verify_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
         
 def send_email(message : str , subject : str , to_email : str):
     pass
 
-def authenticate(token: Request):
-    token = token.headers.get("Authorization")
+def authenticate(token: str = Depends(oauth2_scheme)):
     if token is None or not token.startswith("Bearer "):
         raise HTTPException(status_code=401, detail="Unauthorized")
     token = token.split(" ")[1] 
     
     
-    data = verify_access_token(token)
-    return data['user']
+    data = verify_token(token)
+    return data
