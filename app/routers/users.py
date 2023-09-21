bf2f63d07c4ff749bc86098323087cd5d325c928
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-09-21 23:30:29 +0100

    New: /api/v1/user/all to get all users attached to the currently logged in user

--- app/routers/users.py
+++ app/routers/users.py
@@ -3,7 +3,10 @@ from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 from app.middleware.authenticate import authenticate
 from app.db.database import get_db
-from app.services.user_services import  search_user_by_name_or_email
+from app.services.user_services import (
+    search_user_by_name_or_email,
+    get_org_users
+)
 from app.models.user_models import User
 from app.Responses.response import UserResponse, UserSearchResponse
 
@@ -16,11 +19,23 @@ async def user_profile(current_user: User = Depends(authenticate)):
     return {"message": "User data fetched successfully",
             "statusCode": 200,
             "data": jsonable_encoder(current_user)}
-            
+
+
 @app.get("/user/search/{name_or_email}", response_model=UserSearchResponse)
 async def search(name_or_email: str, db: Session = Depends(get_db), current_user: User = Depends(authenticate)):
     try:
         users = search_user_by_name_or_email(db, name_or_email)
-        return UserSearchResponse(message= "User search successful", statusCode= 200, data=jsonable_encoder( users))
+        return UserSearchResponse(message="User search successful", statusCode=200, data=jsonable_encoder(users))
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal server error: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Internal server error: {str(e)}")
+
+
+@app.get("/user/all", response_model=UserSearchResponse)
+def all_org_users(db: Session = Depends(get_db), current_user: User = Depends(authenticate)):
+    try:
+        users = get_org_users(db, current_user.org_id)
+        return UserSearchResponse(message="User search successful", statusCode=200, data=jsonable_encoder(users))
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Internal server error: {str(e)}")
