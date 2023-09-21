0b00129a1151a7597b251ea500cd812de2bc34b3
psych0wh1z <ikeagudike@gmail.com>
2023-09-21 16:07:39 +0100

    Add Lunch schemas

---
+++ app/schemas/lunch_schemas.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel
+
+
+class LunchData(BaseModel):
+    receiver_id: id,
+	sender_id: id,
+    quantity: int,
+	redeemed: bool,
+	note: str,
+	created_at: str,
+	id: str
+
+
+class GetLunchResponse(BaseModel):
+    message: str,
+    status_code: int,
+    data: LunchData
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "message": "Lunch request created successfully",
+                "statusCode": 201,
+                "data": {
+		            "receiverId": 11,
+    		        "senderId": 7,
+	                "quantity": 2,
+		            "redeemed": False,
+	                "note": "Special instructions for the lunch",
+		            "created_at": "2023-09-21T15:56:41.716282",
+    		        "id": 55
+	            }
+            }
+        }
