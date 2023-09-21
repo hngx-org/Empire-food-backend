6900d9a81fb0fe31708741f195e04039151480a2
Theresa Sunday <72208328+Resa200@users.noreply.github.com>
2023-09-21 12:47:51 +0100

    Deleted user_db.py and iimplemented functions in user_services.py

--- app/services/user_services.py
+++ app/services/user_services.py
@@ -10,21 +10,56 @@ from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def create_user(db:Session, user:UserCreate):
+
+  """
+    Create a new user and add them to the database.
+    """
+    # Create a new User model instance
+  db_user = User(
+        first_name=user.first_name,
+        last_name=user.last_name,
+        email=user.email,
+        phone=user.phone,
+        password_hash=hash_password(user.password),
+        is_admin=user.is_admin,
+        lunch_credit_balance=user.lunch_credit_balance,
+        refresh_token=user.refresh_token,
+        bank_number=user.bank_number,
+        bank_code=user.bank_code,
+        bank_name=user.bank_name,
+        bank_region=user.bank_region,
+        currency=user.currency,
+        currency_code=user.currency_code,
+    )
+
+      # Add the user to the database session and commit the transaction
+  db.add(db_user)
+  db.commit()
+  db.refresh(db_user)
+
+  return db_user
+
   
-  pass
-  
-
-
-
 
 def get_user(db:Session, user_id:int):
 
-  pass
+  """
+    Get a user by their ID from the database.
+    """
+  user = db.query(User).filter(User.id == user_id).first()
+  return user
+
 
 
 def get_user_by_email(db:Session, email:str):
 
-  pass
+  """
+    Get a user by their email from the database.
+    """
+    # Retrieve the user by email
+  user = db.query(User).filter(User.email == email).first()
+  return user
+
 
 
 def hash_password(password):
