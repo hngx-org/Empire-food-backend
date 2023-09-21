1ec0d78d28ef72b83981701d1c325bd907274b52
FloatinggOnion <jesseosems123@gmail.com>
2023-09-21 18:32:17 +0100

    Update redeem lunch route
    Request takes in a list of IDs, to loop through multiple IDs in a single request. Injected dependency on get_current_user, which depends on authenticate middleware

--- app/routers/lunches.py
+++ app/routers/lunches.py
@@ -1,32 +1,44 @@
-from typing import Annotated
+from typing import Annotated, List
 
 from fastapi import APIRouter, Depends, status
 
 from db.database import get_db
 from models import lunch_models, user_models
 from models.user_models import User
-from services.user_services import get_user_by_token
+from services.user_services import get_current_user
 
 
 router = APIRouter()
 
 
 # Redeem lunch by updating 'redeemed' field
-@router.put('/api/lunch/redeem/:{id}')
-async def redeem_lunch(id: str, user: User = Depends(get_user_by_token), session = Depends(get_db)):
+@router.put('/api/lunch/redeem/:{user_id}')
+async def redeem_lunch(user_ids: List[str], user: User = Depends(get_current_user), session = Depends(get_db)):
+    # get_current_user depends on authenticate 
 
+    for user_id in user_ids:
     # Get lunch obj using id
-    lunch_obj = session.query(lunch_models.Lunch).get(id)
+        lunch_obj = session.query(lunch_models.Lunch).filter(lunch_models.Lunch.receiver_id == user.id).first()
 
-    # Check if current user owns the lunch obj
-    if user.id == lunch_obj.receiver_id:
-        if lunch_obj.redeemed:
-            return "Lunch has already been redeemed"
+        # Check if current user owns the lunch obj
+        if user_id == lunch_obj.sender_id:
+            return "You cannot redeem your own lunch"
+        
+        elif user_id == user.id:
+            if user_id == lunch_obj.receiver_id:
+                if lunch_obj.redeemed:
+                    return "Lunch has already been redeemed"
+                else:
+                    lunch_obj.redeemed = True
+
+                    return {
+                        "message": "Lunch redeemed successfully",
+                        "statusCode": status.HTTP_201_CREATED,
+                        "data": None
+                    }
+                
+            else:
+                return "You cannot redeem this lunch"
+        
         else:
-            lunch_obj.redeemed = True
-
-            return {
-                "message": "Lunch redeemed successfully",
-                "statusCode": status.HTTP_201_CREATED,
-                "data": None
-            }
+            return "You are not authorized to redeem this lunch"
