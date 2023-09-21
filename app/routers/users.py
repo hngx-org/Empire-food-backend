8ba20a3197f4bfc0c6acfc3878b91ba4122e0a81
Uche Ofia <coolucheofia@gmail.com>
2023-09-21 09:24:56 +0100

    feat: Implement basic user signup router

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,10 +1,9 @@
-from fastapi import APIRouter, Depends , HTTPException, Request
-from app.schemas.user_schemas import UserCreate,UserProfileSchema
-from app.services.user_services import create_user,get_current_user
+from fastapi import APIRouter, Depends
 
+from app.schemas.user_schemas import UserCreate, UserProfileSchema
+from app.services.user_services import create_user, get_current_user
 
 app = APIRouter()
-
 
 
 @app.post("/auth/user/signup")
@@ -12,7 +11,7 @@ async def signup(request: UserCreate):
     user, error = create_user(request)
     if error:
         return {"message": error.msg, "statusCode": error.code}
-    
+
     return {"message": "user created successfully", "statusCode": 201, "data": user}
 
 
@@ -21,6 +20,4 @@ async def user_profile(current_user: UserProfileSchema = Depends(get_current_use
     return {"message": "User data fetched successfully",
             "statusCode": 200,
             "data": current_user
-    }
-    
-
+            }
