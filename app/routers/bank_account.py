13bc93b3b7e8ae077f1c3d511b2f0d1cbe7cbdf1
Neon-jeff <omaksteejay@gmail.com>
2023-09-21 09:11:01 +0100

    Added Route for Creating bank details to user details

--- app/routers/bank_account.py
+++ app/routers/bank_account.py
@@ -1,10 +1,23 @@
-from fastapi import APIRouter,Depends
+from fastapi import APIRouter,Depends,HTTPException
 from schemas.bankdetails_schema import BankDetailsCreate,BankDetailsResponse
-router=APIRouter(prefix="api/user/bank")
 from db.database import get_db
 from sqlalchemy.orm import Session
+from middleware.authenticate import authenticate
+from models.user_models import User
 
 
+# This Endpoint was written by Neon
+
+router=APIRouter(prefix="api/user/bank")
 @router.post("")
-def create_bank_details(data:BankDetailsCreate,db:Session=Depends(get_db)):
-    pass
+def create_bank_details(data:BankDetailsCreate,db:Session=Depends(get_db),user_id:int=Depends(authenticate)):
+    # query the user model to retrieve the authenticated user
+    auth_user=db.query(User).filter(User.id==user_id)
+    if not auth_user.first():
+        raise HTTPException(
+            403,detail={"Unauthenticated":"User not found"}
+        )
+    else:
+        auth_user.update(data.dict(),synchronize_session=False)
+        return {"message":"Successfully created bank account","StatusCode":200}
+
