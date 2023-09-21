72c185ee22a9bbb50196355f2982dd78950c1042
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-09-21 10:15:22 +0100

    Complete working deployment script

--- deploy/service.sh
+++ deploy/service.sh
@@ -1,23 +1,21 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Replace these variables with your specific values
 APP_NAME="empire_free_lunch"
 USER="mypythtesting"
-APP_DIRECTORY="/app/backend"
-# VENV_PATH="/app/backend/venv"
+APP_DIRECTORY="/home/mypythtesting/empire_backend"
+VENV_PATH="/home/mypythtesting/empire_backend/.venv"
 UVICORN_OPTIONS="--host 0.0.0.0 --port 8000"
-
 # Create a systemd service unit file
 echo "[Unit]
 Description=Your FastAPI Application
 [Service]
 User=$USER
 WorkingDirectory=$APP_DIRECTORY
-ExecStart=pipenv shell && uvicorn main:app $UVICORN_OPTIONS
+ExecStart=$VENV_PATH/bin/uvicorn main:app $UVICORN_OPTIONS
 Restart=always
 [Install]
 WantedBy=multi-user.target" | sudo tee /etc/systemd/system/$APP_NAME.service > /dev/null
-
 # Reload systemd
 sudo systemctl daemon-reload
 # Start and enable the service
