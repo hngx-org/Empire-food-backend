a8a5f2072557e32369c47cf4fff45a41446bce87
Neon-jeff <omaksteejay@gmail.com>
2023-09-22 11:26:46 +0100

    Fixed print code

--- app/routers/bank_account.py
+++ app/routers/bank_account.py
@@ -18,7 +18,7 @@ def get_account_details(db:Session=Depends(get_db),user:User=Depends(authenticat
             raise HTTPException(
             403,detail={"Unauthenticated":"User not found"}
         )
-    return {"message":"Bank details","StatusCode":200,"data":{"bank_name":user.bank_name,"bank_number":user.bank_number}}
+    return {"message":"Bank details","StatusCode":200,"data":{"bank_name":user.bank_name,"bank_number":user.bank_number,"bank_code":user.bank_code}}
 
 
 @router.post("")
@@ -30,7 +30,6 @@ def create_bank_details(data:BankDetailsCreate,db:Session=Depends(get_db),user:U
             404,detail={"Unauthenticated":"User not found"}
         )
     else:
-        print(user)
         user.bank_name=data.bank_name
         user.bank_code=data.bank_code
         user.bank_number=data.bank_number
