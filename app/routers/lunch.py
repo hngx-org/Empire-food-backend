cb67e1df16488ce18b9f711b59e5e3efeeb8f087
psych0wh1z <ikeagudike@gmail.com>
2023-09-22 01:52:50 +0100

    Fix lunch router and streamline response schema

--- app/routers/lunch.py
+++ app/routers/lunch.py
@@ -5,35 +5,17 @@ from sqlalchemy.orm import Session
 from app.db.database import get_db
 from app.middleware.authenticate import authenticate
 from app.models.user_models import User
-from app.schemas.lunch_schemas import GetLunchResponse
+from app.Response.response import GetLunchResponse
 from app.services.lunch_services import fetch_lunch
 
 app = APIRouter()
 
 @app.get("lunch/{lunch_id}", response_model=GetLunchResponse)
-async def get_lunch(lunch_id: int, user_id: int = Depends(authenticate), db: Session = Depends(get_db)):
-    # auth_user = get_user(db, user_id)
-    auth_user=db.query(User).filter(User.id==user_id).first()
-    if not auth_user:
-        raise HTTPException(status_code=401, detail="Error: User not found")
-    else:
+async def get_lunch(lunch_id: int, user: User = Depends(authenticate), db: Session = Depends(get_db)):
+    try:
         lunch = fetch_lunch(db, lunch_id)
-
-    if not lunch:
+        response = jsonable_encoder(lunch)
+    except Exception:
         raise HTTPException(status_code=404, detail="Error: Lunch not found")
     else:
-        response = {
-                    "message": "Lunch request created successfully",
-                    "status_code": 201,
-                    "data": {
-                             "receiver_id": lunch.receiver_id,
-                             "sender_id": lunch.sender_id,
-                             "quantity": lunch.quantity,
-                             "redeemed": lunch.redeemed,
-                             "note": lunch.note,
-                             "created_at": lunch.created_at,
-                             "id": lunch.id
-                            }
-                   }
-
-    return response
+        return response
--- app/schemas/lunch_schemas.py
+++ app/schemas/lunch_schemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 
-class LunchResponseData(BaseModel):
+class LunchResponseSchema(BaseModel):
     receiver_id: int
     sender_id: int
     quantity: int
@@ -9,26 +9,3 @@ class LunchResponseData(BaseModel):
     note: str
     created_at: str
     id: int
-
-
-class GetLunchResponse(BaseModel):
-    message: str
-    status_code: int
-    data: LunchResponseData
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "message": "Lunch request created successfully",
-                "status_code": 201,
-                "data": {
-                    "receiverId": 11,
-                    "senderId": 7,
-                    "quantity": 5,
-                    "redeemed": False,
-                    "note": "Special instructions for the lunch",
-                    "created_at": '2023-09-21T15:56:41.716282',
-                    "id": 22
-                }
-            }
-        }
