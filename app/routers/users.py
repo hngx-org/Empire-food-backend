7cb264aff4f1c040a456d91993964c1e7d6070dc
delaworla <worlanuel@hotmail.com>
2023-09-22 10:07:19 +0000

    get all users endpoint

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter , HTTPException, Request
+from fastapi import APIRouter , HTTPException, Request,Depends, status
 from app.schemas.user_schemas import UserCreate
 from app.services.user_services import create_user
 from app.schemas.user_schemas import UserCreate
 from app.services.user_services import create_user
+from app import schemas, models
+from sqlalchemy import asc
+from app.db.database import engine, get_db
+from sqlalchemy.orm import Session
 
 app = APIRouter()
 
@@ -15,5 +19,8 @@ async def signup(request: UserCreate):
     
     return {"message": "user created successfully", "statusCode": 201, "data": user}
 
-    
 
+@app.get("/api/user", response_model = schemas.UserResponseSchema, status_code=status.HTTP_200_OK)
+def get_persons(db:Session = Depends(get_db)):
+    users = db.query(models.User).order_by(asc(models.User.id)).all()
+    return {"message": "All users", "statusCode": 200, "data": users}
