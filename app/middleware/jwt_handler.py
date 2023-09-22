5bc3e175d65dd21f150741f7df795143b72d2c2d
Oluwaseye Owoborode <owoborodeseye@gmail.com>
2023-09-22 01:30:51 +0100

    Fix: (#55)
    - user response
    - user schema
    - authentication

--- app/middleware/jwt_handler.py
+++ app/middleware/jwt_handler.py
@@ -25,6 +25,7 @@ def create_refresh_token(id : int):
 
 
 def verify_token(token: str) -> int:
+
     try:
         data = jwt.decode(token, setting.secret_key,setting.algorithm)
         expire = data.get("expires")
@@ -37,6 +38,7 @@ def verify_token(token: str) -> int:
             raise HTTPException(
                 status_code=status.HTTP_403_FORBIDDEN,
                 detail="Token expired!") 
+        print(data)
         return data['user']
     except JWTError:
         raise HTTPException(
@@ -45,19 +47,19 @@ def verify_token(token: str) -> int:
         )
         
 
-def refresh_access_token(refresh_token, db: Session = Depends(get_db)):
-    id = verify_token(refresh_token)
+def refresh_access_token(user_id:int, refresh_token: str, db: Session):
 
-    user = db.query(User).filter(User.id == id ).first()
+    user = db.query(User).filter(User.id == user_id ).first()
 
     if not user:
         raise HTTPException(detail="User does not exist.", status_code=404)
     elif user.refresh_token != refresh_token:
+
         raise HTTPException(
             detail="Invalid refresh token",
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         )
 
-    return create_access_token(id)
+    return create_access_token(user_id)
     
 
