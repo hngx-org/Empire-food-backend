fcda7a387021b92d9b0ca2f78d13d1cfee2b09e0
owoborode oluwaseye <owoborodeseye@gmail.com>
2023-09-22 15:39:46 +0100

    create organization and update organization lunch price

--- app/schemas/organization_schemas.py
+++ app/schemas/organization_schemas.py
@@ -6,12 +6,13 @@ from pydantic import BaseModel
 class CreateOrganizationSchema(BaseModel):
     organization_name: str
     lunch_price: Optional[float] = 1000.00
-
+    currency_code: Optional[str] = 'NGN'
 
 class OrganizationSchema(BaseModel):
     id: int
     name: str
     lunch_price: Optional[float] = 1000.00
+    currency_code: Optional[str] = 'NGN'
 
     class Config:
         from_attributes = True
