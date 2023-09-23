f0a45c07d1b3512bfe793bcd95a2a1e09a54ac73
Oiseh Omokheoa <oisehzeme@gmail.com>
2023-09-23 09:10:15 +0100

    conflicts

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -1,8 +1,14 @@
+from decouple import config
+
 class Settings:
     secret_key: str = "HI5HL3V3L$3CR3T"
     algorithm: str = "HS256"
     access_tok_expire_minutes: int = 3600
     refresh_tok_expire_minutes: int = 3600
+    email_api_key: str = config("EMAIL_API_KEY")
+    email_api_secret: str = config("EMAIL_API_SECRET_KEY")
+    OTP_INTERVAL : int = config("OTP_INTERVAL")
+    FRONTEND_URL : str = config("FRONTEND_URL")
     api_key: str = "b31050ab8f0e78989057784bce049c6d"
     api_secret: str = "03a2681aebcca8d587135ceb781e8d0c"
 
